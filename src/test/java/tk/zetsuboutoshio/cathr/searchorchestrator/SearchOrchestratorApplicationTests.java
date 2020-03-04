1b4ddd90cede4a6d23c912068343168d05bd6287
Zetsubou Toshio <zetsubou.toshio@gmail.com>
2020-03-04 12:07:33 +0300

    test profile

--- src/test/java/tk/zetsuboutoshio/cathr/searchorchestrator/SearchOrchestratorApplicationTests.java
+++ src/test/java/tk/zetsuboutoshio/cathr/searchorchestrator/SearchOrchestratorApplicationTests.java
@@ -1,12 +1,17 @@
 package tk.zetsuboutoshio.cathr.searchorchestrator;
 
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
+@Import({SearchOrchestratorApplication.class})
+@AutoConfigureEmbeddedDatabase(beanName = "dataSource")
 public class SearchOrchestratorApplicationTests {
 
     @Test
